bb244093d16495159a927e3ec9b5d0fa86b5d8e7
Gabriel-MarcoSilva <gabrielmarkolinosilva14@gmail.com>
2023-05-11 09:27:34 -0300

    feat: start project

---
+++ src/main/java/com/intensivao/intense/IntenseApplication.java
@@ -0,0 +1,13 @@
+package com.intensivao.intense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntenseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/intensivao/intense/entites/game.java
@@ -0,0 +1,117 @@
+package com.intensivao.intense.entites;
+
+import java.util.Objects;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TB_GAME")
+public class game {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="game_name")
+    private String name;
+
+    @Column(name="game_year") //transforma o nome da variável pra game_year
+    private Integer year;
+    private String genre;
+    private Double score;
+    private String imgUrl;
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+    private String platforms;
+
+    public game(){
+
+    }
+
+    public game(long id, String name, Integer year, String genre, String imgUrl, String shortDescription, String longDescription, String platforms){
+        setId(id);
+        setName(name);
+        setYear(year);
+        setImgUrl(imgUrl);
+        setGenre(genre);
+        setShortDescription(shortDescription);
+        setLongDescription(longDescription);
+        setPlatforms(platforms);
+        setScore(score);;
+    }
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+    public String getLongDescription() {
+        return longDescription;
+    }
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+    public String getShortDescription() {
+        return shortDescription;
+    }
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+    public String getImgUrl() {
+        return imgUrl;
+    }
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+    public Double getScore() {
+        return score;
+    }
+    public void setScore(Double score) {
+        this.score = score;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public Integer getYear() {
+        return year;
+    }
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    //verifica se um jogo é igual ao outro
+    @Override
+    public int hashCode(){
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) return true;
+        if(obj == null) return false;
+        if(getClass() != obj.getClass()) return false;
+        
+        game other = (game) obj;
+        return Objects.equals(id, other.id);
+    }
+}
